1202400666503c748561752e79aed11bc57a3ebc
bb2002 <5252bb@daum.net>
2022-08-03 22:28:00 +0900

    [ADD] 디비, 콘피그, 로그 기본 환경설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as winston from 'winston';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQL_DATABASE_HOST'),
+        port: configService.get('MYSQL_DATABASE_PORT'),
+        username: configService.get('MYSQL_DATABASE_USER'),
+        password: configService.get('MYSQL_DATABASE_PASS'),
+        database: configService.get('MYSQL_DATABASE_NAME'),
+        entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+      }),
+      inject: [ConfigService],
+    }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('SpecialFunc', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
